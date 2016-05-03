864d54f0342825d8ead05024d2d4f0d9e18136b7
Amanda Myers <elfgirl1981@yahoo.com>
2016-05-03 18:22:56 +0000

    Adds images

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -54,14 +54,13 @@ class PinsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+   
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
 
